3cab28c81a99bf640a6b39edd6de3b7046d3119a
Bartek Lichaj <Bartek Lichaj>
2016-11-30 20:08:35 +0100

    zadanie 8.6

---
+++ zadanie 8.6/js/scripts.js
@@ -0,0 +1,19 @@
+    function getTriangleArea(a, h) {
+
+		if ( ( a <= 0 ) || ( h <= 0 ) ) {
+		    console.log('Nieprawidłowe dane'); 
+		} else {
+			return ( a*h/2 );
+		}	
+	}	
+		console.log(getTriangleArea(10,6));
+
+	var triangle1Area = getTriangleArea(10, 15);
+		console.log(triangle1Area);
+
+	var triangle2Area = getTriangleArea(8, 9);
+		console.log(triangle2Area);
+
+	var triangle3Area = getTriangleArea(0, 5);
+		console.log(triangle3Area);
+
