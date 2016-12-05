937dccb8b5b6c7f8f771360cc235a32b6ef7f372
Bartek Lichaj <Bartek Lichaj>
2016-12-05 16:46:36 +0100

    zadanie 8.9

---
+++ zadanie 8.9/js/scripts.js
@@ -0,0 +1,12 @@
+function rysujChoinkę(rows) {
+	for (var i = 1 ; i <= rows; i++) {
+		var star = '';
+		for (var n = 1 ; n <= i*2-1; n++) {
+			star += '*';
+		}
+		
+		
+		console.log(star)
+	}
+}
+rysujChoinkę(5);
