b01f91b14a2209de69dd289423f69432052a9ad7
Michał Dolny <michal.dolny@getresponse.com>
2016-12-08 23:21:12 +0100

    ujednolicenie spacji

--- zadanie 8.9/js/scripts.js
+++ zadanie 8.9/js/scripts.js
@@ -1,11 +1,11 @@
 function rysujChoinke(rows) {
-	for (var i = 1 ; i <= rows; i++) {
+	for (var i = 1; i <= rows; i++) {
 		var star = '';
-		for (var n = 1 ; n <= i*2-1; n++) {
+		for (var n = 1; n <= i*2-1; n++) {
 			star += '*';
 		}
-		
-		
+
+
 		console.log(star)
 	}
 }
