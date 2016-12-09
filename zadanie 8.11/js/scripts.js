baff5f5b83892eba5cbddee2845b9a9491ea6996
Bartek Lichaj <Bartek Lichaj>
2016-12-09 11:07:59 +0100

    dodanie przecinka i srednika

--- zadanie 8.11/js/scripts.js
+++ zadanie 8.11/js/scripts.js
@@ -1,6 +1,6 @@
 
-var list = document.getElementById('Lista')
-	add = document.getElementById('addElem')
+var list = document.getElementById('Lista'),
+	add = document.getElementById('addElem');
 
 add.addEventListener('click', function() {
 	list.innerHTML += '<li>item ' + itemsByTagName.length + '</li>';
